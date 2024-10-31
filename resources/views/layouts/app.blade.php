ab67361b3f8d6aa1d7b24a6d88accf8695dfbdf3
kaewtar <kaewtarcmu@gmail.com>
2024-10-31 23:40:12 +0700

    navbar is done

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,8 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <script src="//unpkg.com/alpinejs" defer></script>
+
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,3 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>DataBase Project</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        <link href='https://fonts.googleapis.com/css?family=Aboreto' rel='stylesheet'>
+    </head>
+<body>
+    
 <nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -11,20 +26,15 @@
                 </div>
 
                 <style>
-                    /* .logo {
-                        font-family: 'Times New Roman', serif;
-                        font-size: 24px;
-                        letter-spacing: 5px;
-                        font-weight: bold;
-                        transition: color 0.3s ease; /* Smooth transition for color change */
-                    /* } */
+                
                     .logo {
-                        font-family: "Aboreto"; 
+                        font-family:"Aboreto"; 
                         font-size: 2rem;
                         letter-spacing: 1px;
                         font-weight: bold;
                         transition: color 0.3s ease; /* Smooth transition for color change */
                     }
+
                     /* Light mode styles */
                     @media (prefers-color-scheme: light) {
                         .logo {
@@ -38,47 +48,54 @@
                             color: white; /* White text for dark mode */
                         }
                     }
+
+                    .font{
+                        font-family:"Aboreto"; 
+                        font-weight: bold;
+                    }
                 </style>
                 @if (Route::has('login'))
-                        <nav class="flex space-x-4">
+                        <nav class="flex space-x-4 ml-auto font">
                         @auth
                                 <!-- Product Links -->
                                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                                    <x-nav-link :href="route('display_product')" :active="request()->routeIs('display_product')">
+                                    <x-nav-link :href="route('display_product')" :active="request()->routeIs('display_product')" style="text-decoration: none">
                                         {{ __('Product') }}
                                     </x-nav-link>
                                 </div>
                                 
-                                <!-- Dashboard Links -->
-                                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                                        {{ __('Member Information') }}
-                                    </x-nav-link>
-                                </div>
+                                
 
                                 <!-- Cart Links -->
                                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                                    <x-nav-link :href="route('cart.display_cart')" :active="request()->routeIs('cart.display_cart')">
+                                    <x-nav-link :href="route('cart.display_cart')" :active="request()->routeIs('cart.display_cart')" style="text-decoration: none">
                                         {{ __('Cart') }}
                                     </x-nav-link>
                                 </div>
 
                                 <!-- Summary Links -->
                                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                                    <x-nav-link :href="route('summary.display_summary')" :active="request()->routeIs('summary.display_summary')">
+                                    <x-nav-link :href="route('summary.display_summary')" :active="request()->routeIs('summary.display_summary')" style="text-decoration: none">
                                         {{ __('Summary') }}
+                                    </x-nav-link>
+                                </div>
+
+                                <!-- Dashboard Links -->
+                                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" style="text-decoration: none">
+                                        {{ __('Member Information') }}
                                     </x-nav-link>
                                 </div>
                             
                         @else
                             <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                                <x-nav-link :href="route('login')" :active="request()->routeIs('login')">
+                                <x-nav-link :href="route('login')" :active="request()->routeIs('login')" style="text-decoration: none">
                                     {{ __('Log in') }}
                                 </x-nav-link>
                             </div>
                             @if (Route::has('register'))
                             <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                                <x-nav-link :href="route('register')" :active="request()->routeIs('register')">
+                                <x-nav-link :href="route('register')" :active="request()->routeIs('register')" style="text-decoration: none">
                                     {{ __('Register') }}
                                 </x-nav-link>
                             </div>
@@ -87,61 +104,46 @@
                     </nav>
                 @endif  
             </div>
-            
+          
             <!-- Settings Dropdown -->
-            <div class="hidden sm:flex sm:items-center sm:ms-6">
-                @if (Route::has('login'))
-                    @auth
-                    <x-dropdown align="right" width="48">
-                        <x-slot name="trigger">
-                            <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
-                                <div>{{ Auth::user()->name }}</div>
+            <div class="shrink-0 flex items-center justify-start"> 
+                <x-dropdown align="right" width="48">
+                    <x-slot name="trigger">
+                        <!-- <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150"> -->
+                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
+                            <div>{{ Auth::user()->name }}</div>
+                            <div class="ms-1">
+                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                </svg>
+                            </div>
+                        </button>
+                    </x-slot>
 
-                                <div class="ms-1">
-                                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                    </svg>
-                                </div>
-                            </button>
-                        </x-slot>
+                    <x-slot name="content">
+                        <x-dropdown-link :href="route('profile.edit')" style="text-decoration: none">
+                            {{ __('Profile') }}
+                        </x-dropdown-link>
 
-                        <x-slot name="content">
-                            <x-dropdown-link :href="route('profile.edit')">
-                                {{ __('Profile') }}
+                        <!-- Authentication -->
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+
+                            <x-dropdown-link :href="route('logout')"
+                                onclick="event.preventDefault(); this.closest('form').submit();" style="text-decoration: none">
+                                {{ __('Log Out') }}
                             </x-dropdown-link>
-
-                            <!-- Authentication -->
-                            <form method="POST" action="{{ route('logout') }}">
-                                @csrf
-
-                                <x-dropdown-link :href="route('logout')"
-                                        onclick="event.preventDefault();
-                                                    this.closest('form').submit();">
-                                    {{ __('Log Out') }}
-                                </x-dropdown-link>
-                            </form>
-                        </x-slot>
-                    </x-dropdown>
-                    @endauth
-                @endif
-            </div>
-           
-            <!-- Hamburger -->
-            <div class="-me-2 flex items-center sm:hidden">
-                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out">
-                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </button>
-            </div>
+                        </form>
+                    </x-slot>
+                </x-dropdown>      
+            </div>   
         </div>
     </div>
 
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" style="text-decoration: none">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
         </div>
@@ -157,7 +159,7 @@
             @endauth
         @endif
             <div class="mt-3 space-y-1">
-                <x-responsive-nav-link :href="route('profile.edit')">
+                <x-responsive-nav-link :href="route('profile.edit')" style="text-decoration: none">
                     {{ __('Profile') }}
                 </x-responsive-nav-link>
 
@@ -167,7 +169,7 @@
 
                     <x-responsive-nav-link :href="route('logout')"
                             onclick="event.preventDefault();
-                                        this.closest('form').submit();">
+                                        this.closest('form').submit();" style="text-decoration: none">
                         {{ __('Log Out') }}
                     </x-responsive-nav-link>
                 </form>
@@ -175,3 +177,5 @@
         </div>
     </div>
 </nav>
+</body>
+</html>
